cbf63f93e873b7d7eb6124e6b2270a473919c217
Isaac <48895941+IsaacFigNewton@users.noreply.github.com>
2024-12-02 18:00:47 -0800

    started project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module aaqz
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// focus on developing:
+	//	a representation of an AST
+	// 	the interp function
+	//	the parsing function (if you have time left)
+	fmt.Println("Implement AAQZ5's interpreter here.")
+}
